ff3e6abc483f686477e0b6c34f51eb95853773a2
Vlad-CSA <satdvlad@gmail.com>
2020-07-18 23:07:50 +0300

    chore: create pixi page component

---
+++ src/app/pixi/pixi.page.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pixi',
+  templateUrl: 'pixi.page.html',
+  styleUrls: ['pixi.page.scss']
+})
+export class PixiPage {
+
+  constructor() {}
+
+}
