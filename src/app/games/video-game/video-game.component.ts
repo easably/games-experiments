7e8260a3e758a785c12623c69a12d823504b088b
Vlad-CSA <satdvlad@gmail.com>
2020-07-28 13:39:30 +0300

    feat: implement basic logic for video component

--- src/app/games/video-game/video-game.component.ts
+++ src/app/games/video-game/video-game.component.ts
@@ -5,10 +5,39 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './video-game.component.html',
   styleUrls: ['./video-game.component.scss'],
 })
-export class VideoGameComponent implements OnInit {
+export class VideoGameComponent {
 
-  constructor() { }
+  private readonly MAX_MISTAKES: number = 3;
+  private readonly VIDEO_DURATION: number = 10;
+  currentMistakes: number = 0;
+  wordLength: number = 10;
+  private STEP: number = (this.VIDEO_DURATION / 2) / this.wordLength;
 
-  ngOnInit() {}
+  _currentTime: number = this.VIDEO_DURATION / 2;
+  get currentTime(): number {
+    return this._currentTime;
+  }
+
+  set currentTime(newTime: number) {
+    console.log('newTime = ', newTime);
+    if (newTime === 0) {
+      alert('you loser');
+    } else if (newTime === this.VIDEO_DURATION) {
+      alert('you winner');
+    }
+    this._currentTime = newTime;
+  }
+
+  constructor() {
+  }
+
+  increase() {
+    this.currentTime += this.STEP;
+  }
+
+  decrease() {
+    this.currentTime -= this.currentTime / (this.MAX_MISTAKES - this.currentMistakes);
+    this.currentMistakes++;
+  }
 
 }
